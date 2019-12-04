1a492d70b331cad3cfc31cbecf3d7bf1e0c95e80
saitchadi <saitchadi@student.cscc.edu>
2019-12-03 22:48:25 -0500

    Initial commit

---
+++ src/edu/cscc/BubbleSort.java
@@ -0,0 +1,19 @@
+package edu.cscc;
+
+public class BubbleSort {
+    public static void sort(String[] list) {
+
+        boolean changed = true;
+        do {
+            changed = false;
+            for (int j = 0; j <= list.length-2; j++)
+                if (list[j].compareTo(list[j + 1])>0) {
+                    //swap list[j] wiht list[j+1]
+                    String temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    changed = true;
+                }
+        } while (changed);
+    }
+}
